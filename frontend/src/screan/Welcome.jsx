a9aca76e2ae1067f1db881796bf49b7e3f9758ba
Temesgen Moges <temu1554@gmail.com>
2024-04-07 19:54:00 +0300

    Welcome page

---
+++ frontend/src/screan/Welcome.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import Lottie from 'lottie-react';
+import animationData from './assets/win.json';
+import animationData2 from './assets/win2.json';
+import { FaSun, FaMoon, FaMoneyCheck, FaMoneyBill, FaShare } from 'react-icons/fa';
+import Card from './commponent/Card';
+import Footer from './commponent/Footer';
+
+ const Welcome = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <div className='bg-[#020917] '>
+      <div className='rounded-b-3xl rounded-t-3xl border-b-2 mb-10'>
+        <div className="text-end pr-20 justify-center pt-[2rem] pb-[4rem]">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transform transition duration-300 ease-in-out hover:scale-110">
+            Start now
+          </button>
+          <div className="absolute top-5 right-5 mt-4 mr-4">
+            {darkMode ? (
+              <FaMoon className="text-white text-2xl" onClick={toggleDarkMode} />
+            ) : (
+              <FaSun className="text-orange-300 text-2xl" onClick={toggleDarkMode} />
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className='flex flex-row pt-10 pb-10 space-x-4 pl-36 pr-20 rounded-b-3xl rounded-t-3xl bg-[#081730]'>
+          <div className="flex flex-col justify-center">
+            <h1 className={`font-bold text-6xl text-${darkMode ? 'white' : 'black'}`}>Welcome</h1>
+            <div className={`flex flex-col justify-center text-${darkMode ? 'white' : 'black'}`}>
+              <p className="text-lg mt-4">
+                Ready to join a vibrant community of players?
+              </p>
+              <p className="text-lg">
+                Compete for top prizes, unlock achievements,
+              </p>
+              <p className="text-lg">
+                collect coins, and customize your avatar!
+              </p>
+            </div>
+          </div>
+          <div className="flex-1 pl-40">
+            <Lottie animationData={animationData} style={{ width: '350px', height: 'auto' }} />
+          </div>
+        </div>
+
+        <div className='flex flex-row space-x-4'>
+          <div className="flex-1 pl-32">
+            <Lottie animationData={animationData2} style={{ width: '350px', height: 'auto' }} />
+          </div>
+          <div className="flex flex-col justify-center pr-40">
+            <h1 className={`font-bold text-5xl text-${darkMode ? 'white' : 'black'}`}>Payment Methods</h1>
+            <div className={`flex flex-col justify-center text-${darkMode ? 'white' : 'black'}`}>
+              <div className="flex items-center mt-4">
+                <FaMoneyCheck className="text-4xl mr-4" />
+                <p className="text-lg">Deposit Money</p>
+              </div>
+              <div className="flex items-center mt-4">
+                <FaMoneyBill className="text-4xl mr-4" />
+                <p className="text-lg">Withdraw Money</p>
+              </div>
+              <div className="flex items-center mt-4">
+                <FaShare className="text-4xl mr-4" />
+                <p className="text-lg">Share Money</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Card/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Welcome;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,32 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
+const Myclass = plugin(function ({ addUtilities }) {
+  addUtilities({
+    ".my-rotate-y-180": {
+      transform: "rotateY(180deg)",
+    },
+    ".preserve-3d": {
+      transformStyle: "preserve-3d",
+    },
+    ".perspective": {
+      perspective: "1000px",
+    },
+    ".backface-hidden": {
+      backfaceVisibility: "hidden",
+    },
+  });
+});
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
-        brightColor:"F4511F",
-        backgroundColor:"b7bca9",
-        lightText:'959595',
+      backgroundImage:{
+        "banner-bg":"url('/public/bannerBg.jpg')"
       }
     },
   },
-  plugins: [],
+  plugins: [Myclass],
 }
-
